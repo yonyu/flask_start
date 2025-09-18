5da7bf912015a48d0d977c404e84d6c02a57d180
Yong Yu <ypyu@hotmail.com>
2025-09-17 18:21:12 -0700

    Add the second route of greet

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__) # take variable name as app by convention; The function ca
 @app.route('/') #decorator; the default page
 def index(): # the function name can be anything, just name it as index()
     return  render_template("index.html") # index.html, but we can't return it directly
+
+@app.route('/greet')
+def greet():
+    return render_template("greet.html", name=request.args.get('name', 'world'))
