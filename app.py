b10596665fb60285de92c914ffbf6abad9597602
Yong Yu <ypyu@hotmail.com>
2025-09-17 21:38:53 -0700

    Use the same route

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__) # take variable name as app by convention; The function call turns file as a flask app
 
-@app.route('/') #decorator; the default page
+@app.route('/', methods=["GET", "POST"]) #decorator; the default page
 def index(): # the function name can be anything, just name it as index()
-    return  render_template("index.html") # index.html, but we can't return it directly
-
-@app.route('/greet', methods=["POST"])
-def greet():
-    return render_template("greet.html", name=request.args.get('name', 'world'))
+    if request.method == "GET":
+        return  render_template("index.html") # index.html, but we can't return it directly
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.form.get('name', 'world'))
